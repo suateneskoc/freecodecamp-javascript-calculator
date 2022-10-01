675c587613a6dac297457d88c373c4cd08f2d3b8
Enes Koç <suateneskoc@gmail.com>
2022-10-02 00:25:53 +0300

    fix: refactor buttons

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+/* eslint-disable no-eval */
+import { useState } from "react";
+
 const numberId = [
   "zero",
   "one",
@@ -11,53 +14,54 @@ const numberId = [
   "nine",
 ];
 
+const operations = [
+  { id: "divide", label: "/", symbol: "/" },
+  { id: "multiply", label: "×", symbol: "*" },
+  { id: "subtract", label: "-", symbol: "-" },
+  { id: "add", label: "+", symbol: "+" },
+];
+
 function App() {
+  const [display, setDisplay] = useState("0");
+  const [history, setHistory] = useState("");
   return (
     <div className="flex min-h-screen flex-col items-center justify-center px-2">
       <div className="grid aspect-[2/3] w-full max-w-sm grid-cols-4 grid-rows-6 gap-1 rounded-lg bg-black p-2 font-mono text-2xl text-white">
-        <div id="display" className="rounded-lg col-span-4 bg-gray-800"></div>
+        <div className="col-span-4 flex flex-col items-end justify-evenly rounded-lg bg-gray-800 px-2">
+          <div className="text-lg text-green-600">{history}</div>
+          <div id="display" className="text-3xl">
+            {display}
+          </div>
+        </div>
+
+        {/* AC */}
         <button
           id="clear"
           className="col-span-3 col-start-1 row-start-2 bg-rose-600 hover:bg-rose-500 active:bg-rose-700"
         >
           AC
         </button>
-        <button
-          id="divide"
-          className="col-span-1 col-start-4 row-start-2 bg-amber-500 hover:bg-amber-400 active:bg-amber-600"
-        >
-          /
-        </button>
-        <button
-          id="multiply"
-          className="col-span-1 col-start-4 row-start-3 bg-amber-500 hover:bg-amber-400 active:bg-amber-600"
-        >
-          ×
-        </button>
-        <button
-          id="subtract"
-          className="col-span-1 col-start-4 row-start-4 bg-amber-500 hover:bg-amber-400 active:bg-amber-600"
-        >
-          -
-        </button>
-        <button
-          id="add"
-          className="col-span-1 col-start-4 row-start-5 bg-amber-500 hover:bg-amber-400 active:bg-amber-600"
-        >
-          +
-        </button>
+
+        {/* Operations */}
+        {operations.map((operation, index) => (
+          <button
+            id={operation.id}
+            className="col-span-1 col-start-4 bg-amber-500 hover:bg-amber-400 active:bg-amber-600"
+            style={{ gridRowStart: index + 2 }}
+          >
+            {operation.label}
+          </button>
+        ))}
+
+        {/* Equals */}
         <button
           id="equals"
           className="col-span-1 col-start-4 row-start-6 bg-blue-600 hover:bg-blue-500 active:bg-blue-700"
         >
           =
         </button>
-        <button
-          id="equals"
-          className="col-span-1 col-start-4 row-start-6 bg-blue-600 hover:bg-blue-500 active:bg-blue-700"
-        >
-          =
-        </button>
+
+        {/* Numbers */}
         {Array.from(Array(9).keys()).map((i) => {
           return (
             <button
@@ -72,12 +76,16 @@ function App() {
             </button>
           );
         })}
+
+        {/* Zero */}
         <button
           id={numberId[0]}
           className="col-span-2 col-start-1 row-start-6 bg-gray-500 hover:bg-gray-400 active:bg-gray-600"
         >
           0
         </button>
+
+        {/* Decimal */}
         <button
           id="decimal"
           className="col-span-1 col-start-3 row-start-6 bg-gray-500 hover:bg-gray-400 active:bg-gray-600"
